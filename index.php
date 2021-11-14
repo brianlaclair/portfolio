3e9b7cead2ce4ee34fe30c5b4eb3d4c1cc862a72
brianlaclair <65574765+brianlaclair@users.noreply.github.com>
2021-11-14 14:47:26 -0500

    update

--- index.php
+++ index.php
@@ -3,11 +3,27 @@
 <link rel="stylesheet" href="style.css">
 </HEAD>
 <BODY>
-<DIV id="overlay" class="hiddenstart"><a onclick="closeModal()" id="up" class="navbutton cornerfix">X</a>
+<DIV id="overlay" class="hiddenstart">
+<a onclick="closeModal()" id="up" class="navbutton cornerfix">X</a>
 <div class="overlaycontent">
-<h2>Wolfpack Island</h2>
+
+<div class="grid-overlay-container">
+<div class="overlay-left"><img id="overlayimage-1" src="" class="overworks"/></div>
+<div class="overlay-mid"><img id="overlayimage-2" src="" class="overworks"/></div>
+<div class="overlay-right"><img id="overlayimage-3" src="" class="overworks"/></div>
+<div class="overlay-left"><h2 id="overlayname"></h2><b><sub id="overlaytitle"></sub></b><br><sub id="overlaytime"></sub></div>
+<div class="overlay-mid small" id="overlaysect1"></div>
+<div class="overlay-right small" id="overlaysect2"></div>
+</div>
+
+<h2 id="overlayname"></h2>
+<h2 id="overlaytitle"></h2>
+<h2 id="overlaytime"></h2>
+
+
 </div>
 </div>
+
 <DIV id="topsection">
 <DIV id="head">
 <h1>Brian LaClair</h1>
@@ -31,7 +47,7 @@
 <img src="assets/me.jpg"/>
 </div>
 <div class="right">
-<p>Brian discovered his passion for programming at a young age with an old computer and a copy of QBASIC</p><p>Over 15 years later, he works daily designing and developing websites, games, mobile apps, and open-source libraries in a multitude of languages</p><p>When he's not coding, you can find Brian spending time with his friends at local concerts or hanging out with his dogs, cat, or guinea pigs</p>
+<p>Brian discovered his passion for programming at a young age with an old computer and a copy of QBASIC</p><p>Over 15 years later, he works daily designing and developing websites, games, mobile apps, and open-source libraries in a multitude of languages</p><p>When he's not coding, you can find Brian spending time with his friends at local concerts or hanging out with his dogs, cat, and guinea pigs</p>
 </div>
 </div>
 </div>
@@ -66,29 +82,36 @@
 </div>
 
 <script>
-// When the user scrolls the page, execute myFunction
-window.onscroll   = function() {myFunction(); close();};
+// Get root element
+var root = document.documentElement;
 
-var scroll = 0;
+// Setup for nav sticking
+var header 		  	= document.getElementById("nav");
+var hiddenbut 	  	= document.getElementById("goup");
 
-// Get the header
-var header 		  = document.getElementById("nav");
-var hiddenbut 	  = document.getElementById("goup");
+// Setup for overlay
+var overlay			= document.getElementById("overlay");
+var overlayName		= document.getElementById("overlayname");
+var overlayTitle	= document.getElementById("overlaytitle");
+var overlayTime		= document.getElementById("overlaytime");
+var overlayImage1   = document.getElementById("overlayimage-1");
+var overlayImage2   = document.getElementById("overlayimage-2");
+var overlayImage3   = document.getElementById("overlayimage-3");
+var overlaySect1    = document.getElementById("overlaysect1");
+var overlaySect2    = document.getElementById("overlaysect2");
+
+// Get id of the main content section
+var mainContent		= document.getElementById("main")
 
 // Get the offset position of the navbar
-var sticky 	= header.offsetTop;
+var sticky 		  	= header.offsetTop;
 
+// When the user scrolls the page, this helps us check if things need to happen, like sticking the navbar
+window.onscroll   = function() {nav_stick();};
 
-function close() {
-	scroll++;
-	if (scroll > 50) {
-		closeModal();
-		scroll = 0;
-	}
-}
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
+// Sticks to top of the screen if we've reached it, unsticks if we're back to it's original position. Also un-hides the "Home" button.
+function nav_stick() {
+	
   if (window.pageYOffset > sticky) {
     header.classList.add("stickytop");
 	hiddenbut.classList.remove("invisi");
@@ -96,22 +119,45 @@ function myFunction() {
     header.classList.remove("stickytop");
 	hiddenbut.classList.add("invisi");
   }
+  
 }
 
-function openModal() {
-  var currentScroll = window.pageYOffset;
-  scroll = 0;
-  document.getElementById("overlay").classList.add("showoverlay");
-  document.getElementById("overlay").classList.remove("hideoverlay");
-  document.getElementById("overlay").classList.remove("hiddenstart");
+function openModal(name, title, time, img1, img2, img3, section1, section2) {
+	// Prevent scrolling
+	root.className = 'noscroll';
+	
+	// Blur the page behind the overlay for a "glassy" effect
+	mainContent.classList.add("mainblur");
+	header.classList.add("mainblur");
+	
+	// Show the overlay
+	overlay.classList.add("showoverlay");
+	overlay.classList.remove("hideoverlay");
+	overlay.classList.remove("hiddenstart");
+	
+	// Update the contents of the overlay
+	overlayName.textContent 	= name;
+	overlayTitle.textContent	= title;
+	overlayTime.textContent 	= time;
+	overlaySect1.innerHTML		= section1;
+	overlaySect2.textContent 	= section2;
+	overlayImage1.src			= "assets/" + img1;
+	overlayImage2.src			= "assets/" + img2;
+	overlayImage3.src			= "assets/" + img3;
 }
 
 function closeModal() {
-   setTimeout(function() {
-    document.getElementById("overlay").classList.add("hiddenstart");
-  }, 300);
-  document.getElementById("overlay").classList.remove("showoverlay");
-  document.getElementById("overlay").classList.add("hideoverlay");
+	// add hiddenstart after the transition has finished to allow for an animation
+	setTimeout(function() {
+		overlay.classList.add("hiddenstart");
+	}, 300);
+	
+	// Unblur the page and hide the overlay
+	root.className = '';
+	header.classList.remove("mainblur");
+	mainContent.classList.remove("mainblur");
+	overlay.classList.remove("showoverlay");
+	overlay.classList.add("hideoverlay");
 }
 </script>
 </BODY>
--- projects/listProjects.php
+++ projects/listProjects.php
@@ -1,7 +1,7 @@
 <?php
 
 // Get the projects
-$file 		 	= "projects/projects/wpi.json";
+$file 		 	= "projects/data/wpi.json";
 $proj_info	 	= json_decode(file_get_contents($file), true);
 $num 			= 0;
 
@@ -19,12 +19,9 @@ echo @'</div>
 <div class="worksleft">
 ';
 
-echo "<img src=\"assets/{$proj_info['thumb']}\" class=\"works\"/></div><div class=\"right small\">";
+echo "<img src=\"assets/{$proj_info['thumb']}\" class=\"works\"/></div><div class=\"right small worksmod\">";
 
-echo "<h4>{$proj_info['name']}</h4><sub><b>{$proj_info['title']}</b></sub><br><sub>{$proj_info['time']}</sub><br><br>
-<p>{$proj_info['shortDes']}</p>";
-
-echo '<a onclick="openModal()" class="navbutton showmore">Learn More...</a>';
+echo "<h4>{$proj_info['name']}</h4><sub><b>{$proj_info['title']}</b></sub><br><sub>{$proj_info['time']}</sub><br><br><p>{$proj_info['shortDes']}</p> <a onclick=\"openModal('{$proj_info['name']}', '{$proj_info['title']}', '{$proj_info['time']}', '{$proj_info['thumb']}', '{$proj_info['image2']}', '{$proj_info['image3']}', '{$proj_info['section1']}', '{$proj_info['section2']}')\" class=\"navbutton showmore\">Learn More...</a>";
 
 echo @'</div>
 </div>
